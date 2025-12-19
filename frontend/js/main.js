872be8f331c904267e69421100848ad0aed94535
WildcatZack <82242044+WildcatZack@users.noreply.github.com>
2025-12-19 17:51:38 -0500

    Ensure Bootstrap JS bundle and icons are available in dev

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,4 +1,4 @@
-import 'bootstrap';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
 tooltipTriggerList.forEach((tooltipTriggerEl) => {
